b498e05d08d75cde21b9ed0c3086a445dce81ca5
stevenjmarsh <sjm.atl@gmail.com>
2018-10-24 12:42:12 -0400

    adding eslint & prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const path = require("path");
+
+const outputDirectory = "dist";
+
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: __dirname + "/dist",
+    path: path.join(__dirname, outputDirectory),
     publicPath: "/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
-    contentBase: "./dist"
-  }
+    contentBase: `./${outputDirectory}`,
+  },
 };
